493077639496db1934834e157579ce1bf56b55a2
neripark <neripark@gmail.com>
2018-12-08 14:20:49 +0900

    fizz buzz

--- 2sec.rb
+++ 2sec.rb
@@ -9,3 +9,20 @@ puts(int2.to_s)
 puts(str1.class)
 puts(int2.class)
 
+puts('----------')
+
+# 関数は最後に評価された式が戻り値になる（returnが不要）
+def greet(str)
+  if str == 'japan'
+    'こんにちは'
+  else
+    'hoge' == 'hoge'
+  end
+end
+puts greet('')
+
+# 引数が不要な場合、普通はかっこはつけない
+def greet2
+  puts 'yeah.'
+end
+greet2
