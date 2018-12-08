f5a5b07dc519f8c63c280c6c8c1d7c91c4ed5fa9
neripark <neripark@gmail.com>
2018-12-09 02:35:39 +0900

    refactor answer.

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -5,11 +5,18 @@ def to_hex r, g, b
   end
 end
 
+#def to_ints hx
+#  ints = [hx[1, 2], hx[3, 2], hx[5, 2]]
+#  ints.map do |el|
+#    el.to_i(16)
+#  end
+#end
+
+# リファクタ 正規表現で任意の２桁の文字列を取り出して配列にしている
+# hex はrubyの標準メソッド
 def to_ints hx
-  ints = [hx[1, 2], hx[3, 2], hx[5, 2]]
-  ints.map do |el|
-    el.to_i(16)
-  end
+  #ints = hx.scan(/\w\w/).map{|el| el.to_i(16)}
+  ints = hx.scan(/\w\w/).map(&:hex)
 end
 
 p to_hex(12, 3, 5)
