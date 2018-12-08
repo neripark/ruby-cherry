b3b1b0839eb3b794bff7751763bf37d430a4430d
neripark <neripark@gmail.com>
2018-12-09 02:16:46 +0900

    answer

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -1,21 +1,13 @@
 def to_hex r, g, b
   rgb = [r,g,b]
   color_code = rgb.inject('#') do |result, str|
-    result + to_hex_unit(str)
+    result + str.to_s(16).upcase.rjust(2, "0")
   end
 end
 
-def to_hex_unit int
-  int.to_s(16).upcase.rjust(2, "0")
-end
-
-def to_ints hex
-  hex.slice!('#')
-  to_int_unit(hex)
-end
-
-def to_int_unit code
-  ints = [code[0, 2], code[2, 2], code[4, 2]]
+def to_ints hx
+  hx.slice!('#')
+  ints = [hx[0, 2], hx[2, 2], hx[4, 2]]
   ints.map do |el|
     el.to_i(16)
   end
@@ -27,4 +19,5 @@ p to_hex(4, 60, 120)
 p to_ints('#000000')
 p to_ints('#ffffff')
 p to_ints('#043c78')
+
 
